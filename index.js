0e20998eb7b0dcf08bfd252fdfdafdcacb044329
Outlook2530 <terabaap2530@outlook.com>
2025-10-05 03:02:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ try {
 
 // ✅ Group Info (change these)
 const GROUP_THREAD_ID = "1122847669958355";        // Group ka ID
-const LOCKED_GROUP_NAME = "AKHILESH KI MA KI CHUDAI 🐼🤣";     // Locked name
+const LOCKED_GROUP_NAME = "KUNAL + ANJALI MOTHERCHODO AUKAT BNAO 🙄👈";     // Locked name
 
 // ✅ Express Server to keep bot alive (for Render or UptimeRobot)
 const app = express();
