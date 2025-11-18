6c510ea4b1259cd21b4bf4fee7f67af540eb485b
Outlook2530 <terabaap2530@outlook.com>
2025-11-18 17:36:31 +0530

    Update group ID and locked group name

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ try {
 }
 
 // ✅ Group Info (change these)
-const GROUP_THREAD_ID = "721069440497584";        // Group ka ID
-const LOCKED_GROUP_NAME = "खुशी की रण्डी मां चुद कर भाग रही MOTHERCHOD 🐼";     // Locked name
+const GROUP_THREAD_ID = "1064561179024578";        // Group ka ID
+const LOCKED_GROUP_NAME = "अभिराज झांट चमार की मां चुद कर फरार 😂👈";     // Locked name
 
 // ✅ Express Server to keep bot alive (for Render or UptimeRobot)
 const app = express();
